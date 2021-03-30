a7933751a1a14651e56053da3fe37fb0127ec235
Shunya078 <kikukan007@gmail.com>
2021-03-30 20:12:38 +0900

    deploy

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,11 @@ export default {
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
-    customVariables: ["~/assets/scss/_imports.scss"],
+    customVariables: ["~/assets/scss/_vuetify-variables.scss"],
+    treeShake: true,
+    options: {
+      customProperties: true,
+    },
     theme: {
       dark: false,
       themes: {
--- pages/index.vue
+++ pages/index.vue
@@ -116,6 +116,8 @@
 </style>
 
 <script>
+import firebase from "firebase";
+
 export default {
   data() {
     return {
